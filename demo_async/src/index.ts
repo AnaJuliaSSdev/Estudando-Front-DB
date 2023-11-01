b4d11caffec00e78373b59c0715b02dd4dcd4010
Ana Julia <anajuliassdev@gmail.com>
2023-11-01 17:04:29 -0300

    creating main function

---
+++ demo_async/src/index.ts
@@ -0,0 +1,19 @@
+async function main() {
+    const uriBase = 'https://jsonplaceholder.typicode.com';
+    try {
+        const resposta = await fetch(`${uriBase}/posts`); 
+        if(resposta.ok){
+            const dados = await resposta.json(); 
+            console.log(dados); 
+        } else {
+            console.log(resposta.status);
+            console.log(resposta.statusText);  
+        }
+    }  catch(error){
+        console.error('Falha na requisição');
+        console.error(error); 
+    }
+}
+
+main();
+
